1aad6bd025f2db427be80a156fe9995e2076b93c
MelvinBusch <melvin.busch@t-online.de>
2018-07-05 14:47:11 +0200

    Typescript Setup

--- server.ts
+++ server.ts
@@ -1,1 +1,7 @@
-// Server Script
+import * as Http from "http";
+import * as Socket from "./node_modules/@types/socket.io";
+
+const server: Http.Server = Http.createServer(serverInit);
+
+
+function serverInit(): void { }
